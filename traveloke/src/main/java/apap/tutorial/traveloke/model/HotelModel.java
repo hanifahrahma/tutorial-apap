d3cbc34df8a999ad7450bd2f4a7c4b86f845abbe
hanifah.rahmajati <hanifah.rahmajati@ui.ac.id>
2020-10-01 10:58:32 +0700

    feat: tutorial 2 - initialize traveloke project

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/model/HotelModel.java
@@ -0,0 +1,44 @@
+package apap.tutorial.traveloke.model;
+
+public class HotelModel {
+    private  String idHotel;
+    private String namaHotel;
+    private String alamat;
+    private String noTelepon;
+
+    public HotelModel(String idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public String getIdHotel() {
+        return idHotel;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    public String getNamaHotel() {
+        return namaHotel;
+    }
+
+    public void setIdHotel(String idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public void setNamaHotel(String namaHotel) {
+        this.namaHotel = namaHotel;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+}
